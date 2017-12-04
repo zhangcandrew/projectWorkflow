fe2eeebc4aecc33bb1d975e9217c6dbebe2716a1
grantmonty <grantmonty@gmail.com>
2017-12-04 15:33:59 -0500

    Update userproject.server.model.js

--- modules/userprojects/server/models/userproject.server.model.js
+++ modules/userprojects/server/models/userproject.server.model.js
@@ -43,7 +43,7 @@ var UserprojectSchema = new Schema({
     projecttype: {
     type: String,
     default: '',
-    required: 'Please select your projects current progress',
+    required: 'Please select your projects type',
     trim: true
   },
     progress: {
